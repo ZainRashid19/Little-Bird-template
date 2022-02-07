ec2a98ca29127498144f595ca44120b97898d8e4
ZainRashid19 <92735725+ZainRashid19@users.noreply.github.com>
2022-02-07 00:14:03 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 function showForm() {
+    if(document.getElementById("contactForm").style.display === "block")
+    {
     document.getElementById("contactForm").style.display = "none";
+  }
+  else {
+    document.getElementById("contactForm").style.display = "block";)
+    {
   }
